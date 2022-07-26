b25aeb2d5aeefb8309e5eb7666a61bdf96dded30
Regina Santangelo <reggiisantangelo@gmail.com>
2022-07-25 21:00:52 -0300

    contador con boton

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import Counter from './components/Counter/Counter';
 
 function App() {
+
+  const handleOnAdd = (quantity)=> {
+    console.log('La cantidad de productos seleccionados es', quantity)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <Navbar/>
         <ItemListContainer greeting="Bienvenido a mi E-Commerce en desarrollo :)" />
+        <Counter stock={11} initial={1} onAdd={handleOnAdd}/>
       </header>
     </div>
+    
   );
 }
 
---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import './Counter.css'
+
+const Counter = ({stock, initial, onAdd})=>{
+    const [count, setCount] = useState(0)
+
+    const decrement = ()=>{
+        if(count > initial){
+           setCount(count - 1) 
+        }
+    }
+
+    const increment = ()=>{
+        if(count < stock){
+           setCount(count + 1) 
+        }
+    }
+
+    return(
+        <div className="counter">
+            <div className="counter2"><button className="boton" onClick={decrement}>-</button>
+            <p className="number">{count}</p>
+            <button className="boton" onClick={increment}>+</button></div>
+            
+            <button className="agregar" onClick={()=>onAdd(count)}>Agregar al carrito</button>
+        </div>
+    )
+}
+
+export default Counter
